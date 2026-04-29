ec2fc5751ed62f3db9c37433a8d9fb540d445612
nicoly222 <f.nicoly@aluno.ifsp.edu.br>
2026-04-29 07:59:20 -0300

    oi

---
+++ src/test/java/com/ifsp/Auroraprojeto/AuroraprojetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifsp.Auroraprojeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuroraprojetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
